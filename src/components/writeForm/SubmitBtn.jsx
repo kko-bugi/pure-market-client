dd054e45ad4760ee03e7c701b7ee04393cfc5cd3
YeonjuSeo <tjduswn1219@gmail.com>
2024-01-28 20:05:02 +0900

    Design: Add Submit Button for Write Screen - 글 작성 화면에 필요한 제출 버튼을 컴포넌트로 작성

---
+++ src/components/writeForm/SubmitBtn.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+function SubmitBtn({ onClick, bgColor, color = "white" }) {
+  return (
+    <StyledBtn onClick={onClick} bgColor={bgColor} color={color}>
+      작성 완료하기
+    </StyledBtn>
+  );
+}
+
+export default SubmitBtn;
+
+const StyledBtn = styled.button`
+  width: 781px;
+  margin-top: 77px;
+  background-color: ${(props) => props.bgColor};
+  color: ${(props) => props.color};
+  font-size: 18px;
+  outline: none;
+`;
