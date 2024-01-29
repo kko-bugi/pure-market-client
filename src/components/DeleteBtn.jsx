a36756897e732b041ffa29111b0c407be138fea9
YeonjuSeo <tjduswn1219@gmail.com>
2024-01-29 20:57:42 +0900

    Design: Add DeleteBtn as Component for Detailed Screens - 세부내용 화면 용 글 삭제 버튼을 컴포넌트로 작성

---
+++ src/components/DeleteBtn.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+function DeleteBtn({ onClick }) {
+  return <StyledBtn onClick={onClick}>삭제하기</StyledBtn>;
+}
+
+export default DeleteBtn;
+
+const StyledBtn = styled.button`
+  color: #888;
+  font-size: 12px;
+  font-weight: 500;
+  background-color: white;
+  padding: 0;
+  border: none;
+`;
