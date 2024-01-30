d0c0d2615f5ec32fa88db0e806fa7b8adfac5d21
YeonjuSeo <tjduswn1219@gmail.com>
2024-01-30 12:03:43 +0900

    Design: Add Detail Screen for Marekt - 농산물 판매글 상세 화면 작성

---
+++ src/pages/Market/ContentPrice.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+function ContentPrice({ price }) {
+  return (
+    <PriceWrapper>
+      <Price>{price}</Price>원
+    </PriceWrapper>
+  );
+}
+
+export default ContentPrice;
+
+const PriceWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-end;
+  margin-top: 30px;
+`;
+
+const Price = styled.div`
+  font-size: 30px;
+  font-weight: 600;
+  margin-right: 5px;
+`;
