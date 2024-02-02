eaae17973afaf81953f697b99ee71618c7626381
CSE-pebble <CSE.pebble@gmail.com>
2024-02-02 23:55:57 +0900

    Feat: Add value property to password/passwordCofirm - 비밀번호 input란 초기화를 위한 value 속성 추가

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -204,6 +204,7 @@ export default function SignUp() {
             text={{
               type: "password",
               placeholder: "비밀번호를 입력해주세요",
+              value: form.password,
               name: "password",
               onChange: (e) => {
                 handleChange(e);
@@ -216,6 +217,7 @@ export default function SignUp() {
             text={{
               type: "password",
               placeholder: "비밀번호를 한번 더 입력해주세요",
+              value: form.passwordConfirm,
               name: "passwordConfirm",
               onChange: (e) => {
                 handleChange(e);
