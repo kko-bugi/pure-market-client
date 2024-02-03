3a13f98240873f6ec49a08fe59c06ed45199cac8
YeonjuSeo <tjduswn1219@gmail.com>
2024-02-04 08:39:29 +0900

    Feat: Add Redirection according to the AccessToken

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,55 @@
 import * as React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ErrorPage from "./ErrorPage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  redirect,
+} from "react-router-dom";
+import { useRecoilValue } from "recoil";
 
 // Pages
+import ErrorPage from "./ErrorPage";
 import Main from "./pages/index";
 import Login from "./pages/Login/index";
 import Market from "./pages/Market/index";
 import MarketWrite from "./pages/Market/Write";
 import ProductDetailed from "./pages/Market/$id";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Main />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/market",
-    element: <Market />,
-  },
-  {
-    path: "/market/write",
-    element: <MarketWrite />,
-  },
-  {
-    path: "/market/:id",
-    element: <ProductDetailed />,
-    errorElement: <ErrorPage />,
-  },
-]);
+import { accessTokenState } from "./atoms/accessTokenState";
 
 function App() {
+  const accessToken = useRecoilValue(accessTokenState);
+  const loginLoader = async () => {
+    // [TODO] accessToken 받아오기...아니면 유저 정보?
+    if (accessToken === "") {
+      return redirect("/login");
+    }
+    return null;
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/market",
+      element: <Market />,
+    },
+    {
+      path: "/market/write",
+      element: <MarketWrite />,
+      loader: loginLoader,
+    },
+    {
+      path: "/market/:id",
+      element: <ProductDetailed />,
+      errorElement: <ErrorPage />,
+    },
+  ]);
   return (
     <React.StrictMode>
       <RouterProvider router={router} />
