f5daafaeeebd55094fcf23db2536139ea405efa6
CSE-pebble <CSE.pebble@gmail.com>
2024-02-04 00:55:43 +0900

    Design: Add Dynamic CSS to Sold Out Label - 판매 완료된 물건이면 이미지에 "거래 완료" 라벨 생성

--- src/components/detail/ContentImg.jsx
+++ src/components/detail/ContentImg.jsx
@@ -1,10 +1,22 @@
 import styled from "styled-components";
 
 function ContentImg({ src, alt, isSoldOut }) {
-  return <StyledImg src={src} alt={alt} isSoldOut={isSoldOut}></StyledImg>;
+  return (
+    <StyledImgContainer>
+      <StyledImg src={src} alt={alt} isSoldOut={isSoldOut}></StyledImg>
+      {isSoldOut && <SoldOutLabel>거래 완료</SoldOutLabel>}
+    </StyledImgContainer>
+  );
 }
 
 export default ContentImg;
+
+const StyledImgContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  max-height: 416px;
+`;
 
 const StyledImg = styled.img`
   width: 100%;
@@ -12,4 +24,16 @@ const StyledImg = styled.img`
   max-height: 416px;
   object-fit: cover;
   ${(props) => props.isSoldOut && `filter: grayscale(40%); opacity:0.5`}
+`;
+
+const SoldOutLabel = styled.div`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  background-color: rgba(169, 169, 169);
+  padding: 15px 0;
+  color: white;
+  text-align: center;
+  font-size: 18px;
+  font-weight: bold;
 `;
