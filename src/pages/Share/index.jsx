2af7ec9080550df1189c86a7dbea0a5ffecf386e
CSE-pebble <CSE.pebble@gmail.com>
2024-02-03 17:44:12 +0900

    Feat: Add ShareBanner

--- src/pages/Share/index.jsx
+++ src/pages/Share/index.jsx
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 import Template from "../../components/Template";
+import ShareBanner from "./ShareBanner";
 
 const Share = () => {
-  return <Template></Template>;
+  return <Template banner={<ShareBanner />}></Template>;
 };
 
 export default Share;
