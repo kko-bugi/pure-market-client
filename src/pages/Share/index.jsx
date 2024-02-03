524e9e071e0505077bf17b12c01db23323a291a7
CSE-pebble <CSE.pebble@gmail.com>
2024-02-03 18:35:51 +0900

    Docs: Make JSON Data for Share Screen - 나눔 장터 홈 화면 퍼블리싱을 위한 임시 데이터 작성

--- src/pages/Share/index.jsx
+++ src/pages/Share/index.jsx
@@ -3,7 +3,17 @@ import Template from "../../components/Template";
 import ShareBanner from "./ShareBanner";
 
 const Share = () => {
-  return <Template banner={<ShareBanner />}></Template>;
+  return (
+    <Template banner={<ShareBanner />}>
+      <ItemWrapper></ItemWrapper>
+    </Template>
+  );
 };
+
+const ItemWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 23px;
+`;
 
 export default Share;
