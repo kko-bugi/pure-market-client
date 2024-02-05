cfb305fc600bf312678b1226c6d1079e8db8d4ee
CSE-pebble <CSE.pebble@gmail.com>
2024-02-05 10:03:21 +0900

    Feat: Add Routing Page

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,21 @@ import { useRecoilValue } from "recoil";
 import ErrorPage from "./ErrorPage";
 import Main from "./pages/index";
 import Login from "./pages/Login/index";
+import SignUp from "./pages/SignUp/index";
+import MyPage from "./pages/MyPage/index";
 import Market from "./pages/Market/index";
 import MarketWrite from "./pages/Market/Write";
 import ProductDetailed from "./pages/Market/$id";
+import Share from "./pages/Share/index";
+import ShareWrite from "./pages/Share/Write";
+import ShareDetailed from "./pages/Share/$id";
 
 import { accessTokenState } from "./atoms/accessTokenState";
 
 function App() {
   const accessToken = useRecoilValue(accessTokenState);
   const loginLoader = async () => {
-    // [TODO] accessToken ¹Þ¾Æ¿À±â...¾Æ´Ï¸é À¯Àú Á¤º¸?
+    // [TODO] accessToken ï¿½Þ¾Æ¿ï¿½ï¿½ï¿½...ï¿½Æ´Ï¸ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½?
     if (accessToken === "") {
       return redirect("/login");
     }
@@ -36,6 +41,14 @@ function App() {
       element: <Login />,
     },
     {
+      path: "/signUp",
+      element: <SignUp />,
+    },
+    {
+      path: "/myPage",
+      element: <MyPage />,
+    },
+    {
       path: "/market",
       element: <Market />,
     },
@@ -47,6 +60,20 @@ function App() {
     {
       path: "/market/:id",
       element: <ProductDetailed />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/share",
+      element: <Share />,
+    },
+    {
+      path: "/share/write",
+      element: <ShareWrite />,
+      loader: loginLoader,
+    },
+    {
+      path: "/share/:id",
+      element: <ShareDetailed />,
       errorElement: <ErrorPage />,
     },
   ]);
