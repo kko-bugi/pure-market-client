14a1751c8d2fd1df94aea6792a0677747a7585b5
YeonjuSeo <tjduswn1219@gmail.com>
2024-02-14 08:37:23 +0900

    Design: Show SoldOut Label on Product List - 농산물 직거래 마켓 목록 화면에서 거래 완료 여부 표시

--- src/pages/Market/ProductCard.jsx
+++ src/pages/Market/ProductCard.jsx
@@ -11,7 +11,13 @@ function ProductCard({ product }) {
         });
       }}
     >
-      <ItemImg src={product.produceImage} alt="" />
+      <ThumbnailWrapper>
+        <ItemImg src={product.produceImage} alt="" />
+        {product.produceStatus === "판매완료" && (
+          <SoldOutLabel>거래완료</SoldOutLabel>
+        )}
+      </ThumbnailWrapper>
+
       <ItemName>{product.title}</ItemName>
       <ItemPrice>
         {product.price.toLocaleString()}
@@ -27,10 +33,27 @@ const CardWrapper = styled.div`
   max-width: 183px;
   cursor: pointer;
 `;
+const ThumbnailWrapper = styled.div`
+  position: relative;
+`;
 
 const ItemImg = styled.img`
   width: 180px;
   height: 182px;
+  border-radius: 15px;
+`;
+const SoldOutLabel = styled.div`
+  font-size: 24px;
+  font-weight: 600;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.65);
+  color: white;
   border-radius: 15px;
 `;
 
