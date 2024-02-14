27d23b33b83d83b29bd1a2685182e809745e3ff3
YeonjuSeo <tjduswn1219@gmail.com>
2024-02-14 15:26:27 +0900

    Feat: Get User Profile Dynamically - 프로필에 정보가 입력되지 않으면 API 요청을 통해 프로필 정보를 가져오도록 API 연결

--- src/components/MiniProfile.jsx
+++ src/components/MiniProfile.jsx
@@ -1,17 +1,35 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import DefaultImg from "../assets/DefaultProfileImg.png";
+import instance from "../axios_interceptor";
 
-function MiniProfile({
-  profileImg = DefaultImg,
-  nickname = "이름없음",
-  contact = "010-XXXX-XXX",
-}) {
+function MiniProfile({ profileImg, nickname, contact }) {
+  const [img, setImg] = useState(profileImg);
+  const [name, setName] = useState(nickname);
+  const [phone, setPhone] = useState(contact);
+  useEffect(() => {
+    const getUserProfile = async () => {
+      try {
+        const res = await instance.get("/users/profile");
+        console.log(res);
+        setImg(res.data.isSuccess ? res.data.result.profileImage : DefaultImg);
+        setName(res.data.isSuccess ? res.data.result.nickname : "이름없음");
+        setPhone(
+          res.data.isSuccess ? res.data.result.contact : "010-XXXX-XXXX"
+        );
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    if (contact === undefined) getUserProfile();
+  }, []);
+
   return (
     <ProfileWrapper>
-      <ProfileImg src={profileImg} alt="" />
+      <ProfileImg src={img} alt="" />
       <TextWrapper>
-        <Nickname>{nickname}</Nickname>
-        <PhoneNumber>{contact}</PhoneNumber>
+        <Nickname>{name}</Nickname>
+        <PhoneNumber>{phone}</PhoneNumber>
       </TextWrapper>
     </ProfileWrapper>
   );
