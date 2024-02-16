8957e303e40c6253ba576b7e1d4ee09de1d00dc9
Yujeong Lee <144709782+yujeongit@users.noreply.github.com>
2024-02-16 11:25:26 +0900

    Feat: Connect Recipe, Home API

--- src/pages/Recipe/Write.jsx
+++ src/pages/Recipe/Write.jsx
@@ -1,212 +1,130 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom"; 
 import styled from "styled-components";
 import instance from "../../axios_interceptor";
 import { useNavigate } from "react-router-dom";
 
 import Template from "../../components/Template";
 import MiniProfile from "../../components/MiniProfile";
-import WriteInput from "../../components/writeForm/WriteInput";
-import WriteTextArea from "../../components/writeForm/WriteTextArea";
-import SubmitBtn from "../../components/writeForm/SubmitBtn";
-import WriteImgInput from "../../components/writeForm/WriteImgInput";
-import AddContainerIcon from "../../assets/AddContainerIcon.svg";
+import ContentImg from "../../components/detail/ContentImg";
+import ContentTitle from "../../components/detail/ContentTitle";
+import ContentContent from "../../components/detail/ContentContent";
+import DeleteBtn from "../../components/DeleteBtn";
+import ListContent from "../../components/detail/ListContent";
 
-function Write() {
-  const [image, setImage] = useState("");
-  const [recipeRequest, setRecipeRequest] = useState({
-    title: "",
-    content: "",
-    ingredientList: [{ name: '', quantity: '' }],
-    sauceList: [{ name: '', quantity: '' }],
-    recipeDescriptionList: [{ orderNumber: 1, description: '' }]
-  });
 
+  
+function Detailed() {
+  const location = useLocation();
   const navigate = useNavigate();
 
-  const handleTextInputChange = (e, index, type) => {
-    const { name, value } = e.target;
-    if (type === 'ingredient') {
-      const updatedIngredients = [...recipeRequest.ingredientList];
-      updatedIngredients[index][name] = value;
-      setRecipeRequest(prevState => ({
-        ...prevState,
-        ingredientList: updatedIngredients,
-      }));
-    } else if (type === 'sauce') {
-      const updatedSauces = [...recipeRequest.sauceList];
-      updatedSauces[index][name] = value;
-      setRecipeRequest(prevState => ({
-        ...prevState,
-        sauceList: updatedSauces,
-      }));
-    } else if (type === 'order') {
-      const updatedOrders = [...recipeRequest.recipeDescriptionList];
-      updatedOrders[index][name] = value;
-      setRecipeRequest(prevState => ({
-        ...prevState,
-        recipeDescriptionList: updatedOrders,
-      }));
-    } else {
-      setRecipeRequest(prevState => ({
-        ...prevState,
-        [name]: value,
-      }));
-    }
-  };
+  if (location.state === null) {
+    throw Error(404); // 존재하지 않는 페이지
+  }
 
-  const handleAddIngredient = () => {
-    setRecipeRequest(prevState => ({
-      ...prevState,
-      ingredientList: [...prevState.ingredientList, { name: '', quantity: '' }]
-    }));
-  };
-  
-  const handleAddSauce = () => {
-    setRecipeRequest(prevState => ({
-      ...prevState,
-      sauceList: [...prevState.sauceList, { name: '', quantity: '' }]
-    }));
-  };
-  
-  const handleAddOrder = () => {
-    setRecipeRequest(prevState => ({
-      ...prevState,
-      recipeDescriptionList: [...prevState.recipeDescriptionList, { orderNumber: prevState.recipeDescriptionList.length + 1, description: '' }]
-    }));
-  };
+  const recipeIdx = location.state;
+  const [recipeInfo, setRecipeInfo] = useState(null);
 
-  const handleWrite = async (e) => {
-    e.preventDefault();
-  
-    const formData = new FormData();
-    formData.append("image", image);
-
-    formData.append(
-      "recipeRequest",
-      new Blob([JSON.stringify(recipeRequest)], { type: "application/json" })
-    );
-  
-    recipeRequest.ingredientList.forEach((ingredient, index) => {
-      formData.append(`ingredientList[${index}][name]`, ingredient.name);
-      formData.append(`ingredientList[${index}][quantity]`, ingredient.quantity);
-    });
-  
-    recipeRequest.sauceList.forEach((sauce, index) => {
-      formData.append(`sauceList[${index}][name]`, sauce.name);
-      formData.append(`sauceList[${index}][quantity]`, sauce.quantity);
-    });
-  
-    recipeRequest.recipeDescriptionList.forEach((order, index) => {
-      formData.append(`recipeDescriptionList[${index}][orderNumber]`, order.orderNumber);
-      formData.append(`recipeDescriptionList[${index}][description]`, order.description);
-    });
-    
+  const getRecipeData = async () => {
     try {
-      const res = await instance.post("/recipe", formData, {
+      const res = await instance.get(`/recipe/${recipeIdx}`, {
         headers: {
-          "Content-Type": "multipart/form-data",
+          "Content-Type": "application/json",
         },
       });
-      navigate("/recipe");
-      console.log(res);
+      setRecipeInfo(res.data.result);
     } catch (e) {
       console.error(e);
     }
   };
 
+  const handleDelete = async () => {
+    try {
+      await instance.patch(`/recipe/${recipeIdx}`, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      navigate("/recipe");
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    getRecipeData();
+  }, []);
+
+
   return (
     <Template>
-      <ContentWrapper>
+      <Wrapper>
         <Title>레시피 추천 Book</Title>
-        <StyledForm onSubmit={(e) => handleWrite(e)}>
-        <CenterWrapper>
-          <InputsWrapper>
+        {recipeInfo ? (
+          <ContentWrapper>
             <LeftWrapper>
-              <WriteImgInput setImage={setImage} />
+              <ContentImg src={recipeInfo.recipeImage} alt="" />
             </LeftWrapper>
+
             <RightWrapper>
-              <MiniProfile />
-              <WriteInput
-                name="title"
-                placeholder="제목을 입력해주세요."
-                style={{ marginTop: "23px" }}
-                onChange={(e) => handleTextInputChange(e)}
-              />
-              <WriteTextArea
-                name="content"
-                placeholder={"내용을 입력해주세요"}
-                height="80px"
-                style={{ marginTop: "10px", marginBottom: "10px" }}
-                onChange={(e) => handleTextInputChange(e)}
-              />
-              <Subtitle>[재료]</Subtitle>
-              {recipeRequest.ingredientList.map((ingredient, index) => (
-                <IngredientWrapper key={index}>
-                  <WriteInput
-                    name="name"
-                    placeholder="재료명"
-                    style={{ width: "140px", marginRight: "15px" }}
-                    onChange={(e) => handleTextInputChange(e, index, 'ingredient')}
-                  />
-                  <WriteInput
-                    name="quantity"
-                    placeholder="양(ex. 한 숟가락)"
-                    style={{ width: "140px", marginRight: "15px" }}
-                    onChange={(e) => handleTextInputChange(e, index, 'ingredient')}
-                  />
-                  {index === recipeRequest.ingredientList.length - 1 && (
-                    <img src={AddContainerIcon} alt="재료 추가" onClick={handleAddIngredient} />
-                  )}
-                </IngredientWrapper>
-              ))}
-              <Subtitle>[양념]</Subtitle>
-              {recipeRequest.sauceList.map((sauce, index) => (
-                <SauceWrapper key={index}>
-                  <WriteInput
-                    name="name"
-                    placeholder="양념명"
-                    style={{ width: "140px", marginRight: "15px" }}
-                    onChange={(e) => handleTextInputChange(e, index, 'sauce')}
-                  />
-                  <WriteInput
-                    name="quantity"
-                    placeholder="양(ex. 한 숟가락)"
-                    style={{ width: "140px", marginRight: "15px" }}
-                    onChange={(e) => handleTextInputChange(e, index, 'sauce')}
-                  />
-                  {index === recipeRequest.sauceList.length - 1 && (
-                    <img src={AddContainerIcon} alt="양념 추가" onClick={handleAddSauce} />
-                  )}
-                </SauceWrapper>
-              ))}
-            </RightWrapper>
-          </InputsWrapper>
-        </CenterWrapper>
-        <Subtitle2>[조리순서]</Subtitle2>
-        <CenterWrapper>
-            {recipeRequest.recipeDescriptionList.map((order, index) => (
-              <OrderWrapper key={index}>
-                <OrderNumber>{order.orderNumber}.</OrderNumber>
-                <OrderTextArea
-                  name="description"
-                  placeholder={"내용을 입력해주세요 *상세하게 쓸수록 좋아요!"}
-                  height="44px" 
-                  onChange={(e) => handleTextInputChange(e, index, 'order')}
+              <UserWrapper>
+                <MiniProfile
+                  profileImg={recipeInfo.profileImage}
+                  nickname={recipeInfo.nickname}
+                  contact={recipeInfo.contact}
                 />
-                {index === recipeRequest.recipeDescriptionList.length - 1 && (
-                  <img src={AddContainerIcon} alt="조리순서 추가" onClick={handleAddOrder} />
+                {recipeInfo.isWriter && (
+                  <DeleteBtn onClick={handleDelete} />
                 )}
-              </OrderWrapper>
-            ))}
-            <SubmitBtn bgColor="#ffe9c8" color="black"/>
-          </CenterWrapper>
-        </StyledForm>
-      </ContentWrapper>
+              </UserWrapper>
+              <ContentTitle txt={recipeInfo.title} />
+              <ContentContent txt={recipeInfo.content} />
+
+              <SubtitleWrapper>
+                <Subtitle>[재료]&nbsp;</Subtitle>
+                {recipeInfo.ingredientList.map((ingredient, index) => (
+                <React.Fragment key={index}>
+                    <ListContent txt={`${ingredient.name}${ingredient.quantity ? `: ${ingredient.quantity}` : ''}`}  />
+                    {index !== recipeInfo.ingredientList.length - 1 && <>,&nbsp;&nbsp;</>}
+                </React.Fragment>
+                ))}
+              </SubtitleWrapper>
+
+              <SubtitleWrapper>
+                <Subtitle>[양념]&nbsp;</Subtitle>
+                {recipeInfo.sauceList.map((sauce, index) => (
+                  <React.Fragment key={index}>
+                    <ListContent txt={`${sauce.name}${sauce.quantity ? `: ${sauce.quantity}` : ''}`}  />
+                    {index !== recipeInfo.sauceList.length - 1 && <>,&nbsp;&nbsp;</>}
+                </React.Fragment>
+                ))}
+              </SubtitleWrapper>
+
+            </RightWrapper>
+          </ContentWrapper>
+        ) : (
+          <div>로딩중...</div>
+        )}
+
+        {recipeInfo && (
+          <>
+            <Subtitle2>[조리순서]</Subtitle2>
+            <OrderBox>
+              {recipeInfo.recipeDescriptionList.map((step, index) => (
+                <OrderWrapper key={index}>
+                  <OrderNumber>{`${step.orderNumber}.`}&nbsp;</OrderNumber>
+                  <ListContent txt={step.description} />
+                </OrderWrapper>
+              ))}
+            </OrderBox>
+          </>
+        )}
+      </Wrapper>
     </Template>
   );
 }
 
-export default Write;
+export default Detailed;
 
 const Title = styled.div`
   font-size: 34px;
@@ -215,108 +133,70 @@ const Title = styled.div`
   margin-bottom: 32px;
   width: fit-content;
 `;
-const ContentWrapper = styled.div`
+const Wrapper = styled.div`
   width: 100%;
   max-width: 898px;
   height: 100%;
   padding-top: 46px;
 `;
 
-const LeftWrapper = styled.div`
-  background-color: #d9d9d9;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  max-height: 416px;
-`;
-
-const RightWrapper = styled.div`
-  width: 100%;
-`;
-
-const StyledForm = styled.form`
-  margin-top: 23px;
-  display: flex;
-  flex-direction: column;
-`;
-
-const InputsWrapper = styled.div`
+const ContentWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 66px;
   width: 100%;
 `;
 
+const LeftWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  min-height: 416px;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+const RightWrapper = styled.div`
+  width: 100%;
+`;
+
 const Subtitle = styled.div`
-  font-size: 18px;
-  font-weight: 600;
-  margin-bottom: 10px;
-  width: fit-content;
-  text-align: left;
+ font-size: 18px;
+ font-weight: bold;
 `;
 
-const IngredientWrapper = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  margin-bottom: 12px;
-`;
-
-const SauceWrapper = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  margin-bottom: 12px;
+const SubtitleWrapper = styled.div`
+ display: flex;
 `;
 
 const Subtitle2 = styled.div`
-  font-size: 18px;
-  font-weight: 600;
-  margin-bottom: 10px;
-  width: fit-content;
-  text-align: left;
-  margin-left: 50px;
+ font-size: 22px;
+ font-weight: bold;
+ margin-top: 30px;
 `;
 
-const OrderNumber = styled.div`
-  font-size: 18px;
-  font-weight: 600;
-  margin-right: 8px;
-  height: 50px;
-`;
-
-const OrderTextArea = styled.textarea`
-  color: #000;
-  background-color: #fff;
-  border-radius: 5px;
-  border: 1px solid #ddd3d3;
-  padding: 0 19px;
-  width: 100%;
-  max-width: 732px;
-  height: 50px;
-  outline: none;
-  resize: none;
-  margin-bottom: 10px;
-  line-height: 50px; 
-  margin-right: 14px;
+const OrderBox = styled.div`
+ background-color: #ffe9c8;
+ width: 100%;
+ height: 531px; 
+ padding: 20px; 
+ box-sizing: border-box; 
+ margin-top: 10px;
 `;
 
 const OrderWrapper = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  margin-bottom: 10px;
-  margin-left: 100px;
-  width: 100%;
-  max-width: 898px;
+ display: flex;
+ align-items: center; 
+ justify-content: center; 
+ margin-top: 10px;
 `;
 
-
-const CenterWrapper = styled.div`
-  margin-bottom: 23px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+const OrderNumber = styled.div`
+ font-size: 18px;
 `;
 
+const UserWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
