aa4b0262a96d9d4bd2b22221f05a0be22a235551
CSE-pebble <CSE.pebble@gmail.com>
2024-02-17 17:11:28 +0900

    Feat: Connect Share API - 나눔글 목록 조회 API 연결

--- src/pages/Share/index.jsx
+++ src/pages/Share/index.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import styled from "styled-components";
 import Template from "../../components/Template";
 import ShareBanner from "./ShareBanner";
@@ -5,13 +6,28 @@ import data from "../../data/shareList.json";
 import ProductCard from "./ProductCard";
 import Icon from "../../assets/ShareAddIcon.svg";
 import AddIcon from "../../components/AddIcon";
+import { useEffect, useState } from "react";
 
 const Share = () => {
+  const [itemList, setItemList] = useState([]);
+  useEffect(() => {
+    const getShareList = async () => {
+      try {
+        await axios.get("/giveaway").then((res) => {
+          setItemList(res.data.result.giveawayList);
+        });
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    getShareList();
+  }, []);
+
   return (
     <Template banner={<ShareBanner />}>
       <ItemWrapper>
-        {data.items.map((el) => (
-          <ProductCard key={el.product} product={el} />
+        {itemList.map((el) => (
+          <ProductCard key={el.giveawayIdx} product={el} />
         ))}
       </ItemWrapper>
       <AddIcon src={Icon} href="/share/write" />
